266ad52bc2f00281a815fc13e7559caf81c08062
Parag <pcparagc@gmail.com>
2021-02-18 21:52:50 +0530

    Product component of cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+//feature 1
+import React from "react";
 import "./App.css";
+import Products from "./components/Products";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Shoping cart</header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shoping Cart</a>
+        </header>
+        <main className="content">
+          <div className="main">
+            <Products products={this.state.products}></Products>
+          </div>
+          <div className="sidebar">cart items </div>
+        </main>
+        <footer>All rights are reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
